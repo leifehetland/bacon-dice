037f3a20018926c1fb522c46ba758a1b7bb0a8f4
Leif Hetland <lehetland1@gmail.com>
2020-06-16 21:07:08 -0400

    clicking roll dice displays the corresponding dice image

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ var scores = [0,0];
 var roundScore = 0;
 var activePlayer = 1;
 
-var dice = Math.floor(Math.random() * 6)  + 1;
-
-$("#current-" + activePlayer).text(dice);
-
 $(".dice").css("display", "none");
+
+$(".btn-roll").on("click", function () {
+	console.log("clicked");
+	
+	// Random number from 1 to 6
+	dice = Math.floor(Math.random() * 6)  + 1;
+
+	// Display the result
+	$(".dice").css("display", "block");
+	$(".dice").attr("src", "img/dice-"+ dice +".png");
+
+	// Update the round score if the number rolled isn't 1
+
+
+})
