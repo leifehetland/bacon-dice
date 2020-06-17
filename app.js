e6acb8ec50d873015101688f5fd2f3f23fd67fd6
Leif Hetland <lehetland1@gmail.com>
2020-06-16 21:37:20 -0400

    changing active player from 1 to 2 and vice versa with current score resetting if player hits a 1

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ console.log("Hello World");
 
 var scores = [0,0];
 var roundScore = 0;
-var activePlayer = 1;
+var activePlayer = 0;
 
+
+$('#score-0').text('0');
+$('#score-1').text('0');
+$('#current-0').text('0');
+$('#current-1').text('0');
 $(".dice").css("display", "none");
 
 $(".btn-roll").on("click", function () {
@@ -17,6 +22,28 @@ $(".btn-roll").on("click", function () {
 	$(".dice").attr("src", "img/dice-"+ dice +".png");
 
 	// Update the round score if the number rolled isn't 1
+	if (dice !== 1) {
+		// Add score
+		roundScore += dice;
+		$("#current-" + activePlayer).text(roundScore);
+	} else {
+		// Player 2's turn
+		// if active player is set to zero then activePlayer is 1 else active player is 0
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		// Reset current active score
+		$("#current-0").text("0");
+		$("#current-1").text("0");
+
+		// Toggle active class based on player
+		$(".player-0-panel").toggleClass('active');
+		$(".player-1-panel").toggleClass('active');
+
+		// Hide dice for next player
+		$(".dice").css("display", "none");
+
+	}
 
 
 })
