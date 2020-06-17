146e33a51d582928058e80371c51db79cb01718b
Leif Hetland <lehetland1@gmail.com>
2020-06-16 20:06:31 -0400

    set up initial variables for game

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+var scores = [0,0];
+var roundScore = 0;
+var activePlayer = 1;
+
+var dice = Math.floor(Math.random() * 6)  + 1;
+
+$("#current-" + activePlayer).text(dice);
+
+$(".dice").css("display", "none");
